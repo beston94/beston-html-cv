2f09af0efd38dcbb70fea730d7d2d6ad198bf381
Beston Mobasheri <bestonmobasheri9449@gmail.com>
2020-08-13 12:32:13 +0300

    Added event listener to click me

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
 var btn = document.createElement("BUTTON");
-btn.innerHTML = "CLICK ME";                   
-document.body.appendChild(btn); 
+btn.innerHTML = "CLICK ME";
+btn.id = 'beston';
+document.body.appendChild(btn);
+var d = new Date();
+function helloBeston() {
+	alert("Hello Beston");}
+document.getElementById("beston").addEventListener("mouseover", helloBeston);
